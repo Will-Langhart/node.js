ce668084ea7978ce0fff9a98fcdabb7c4010db2e
William Langhart <theholotrout@gmail.com>
2023-11-07 15:49:49 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Please enter something: ', (answer) => {
+    console.log(`You entered: ${answer}`);
+    rl.close();
+});
